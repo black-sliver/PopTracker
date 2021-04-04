3f7cea2546da7b49933c2ee34019013e95d85c9d
black-sliver <>
2021-04-04 20:32:41 +0200

    mark release v0.11.0

--- src/poptracker.h
+++ src/poptracker.h
@@ -47,7 +47,7 @@ public:
     virtual ~PopTracker();
     
     static constexpr const char APPNAME[] = "PopTracker";
-    static constexpr const char VERSION_STRING[] = "0.1.0";
+    static constexpr const char VERSION_STRING[] = "0.11.0";
     
 protected:
     virtual bool start();
