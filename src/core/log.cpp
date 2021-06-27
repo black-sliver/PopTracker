b79f8bd4c6b3c7224bfad199270b4ba0165e7d06
black-sliver <>
2021-06-27 15:41:53 +0200

    logging with no open stdout or stderr (windows)

--- src/core/log.cpp
+++ src/core/log.cpp
@@ -5,6 +5,7 @@ std::string Log::_logFile = "";
 int Log::_oldOut = -1;
 int Log::_oldErr = -1;
 int Log::_newOut = -1;
+int Log::_newErr = -1;
 
 
 #if defined (__unix__) || (defined (__APPLE__) && defined (__MACH__)) || defined(__MINGW32__) || defined(__GNUC__)
@@ -22,6 +23,7 @@ bool Log::RedirectStdOut(const std::string& file, bool truncate) {
     _logFile = file;
     UnredirectStdOut();
     _newOut = open(_logFile.c_str(), O_RDWR|O_CREAT|O_APPEND|(truncate ? O_TRUNC : 0), 0655);
+    _newErr = dup(_newOut);
     if (_newOut == -1) {
         fprintf(stderr, "Could not open %s for logging!\n",
                 _logFile.c_str());
@@ -29,8 +31,21 @@ bool Log::RedirectStdOut(const std::string& file, bool truncate) {
     }
     _oldErr = dup(fileno(stderr));
     _oldOut = dup(fileno(stdout));
-
-    if (dup2(_newOut, fileno(stdout)) == -1) {
+    
+    // if stdout has no underlying handle, use freopen (fileno(stdout) is -2 on windows)
+    if (_oldOut < 0) {
+        freopen(_logFile.c_str(), "w", stdout);
+        close(_newOut);
+        _newOut = fileno(stdout);
+        if (_newOut < 0) {
+            fprintf(stderr, "Could not redirect stdout!\n");
+            close(_oldErr);
+            _oldErr = -1;
+            return false;
+        }
+    }
+    // otherwise have stdout file handle point to file
+    else if (dup2(_newOut, fileno(stdout)) == -1) {
         fprintf(stderr, "Could not redirect stdout!\n");
         close(_newOut);
         close(_oldErr);
@@ -40,23 +55,48 @@ bool Log::RedirectStdOut(const std::string& file, bool truncate) {
         _oldOut = -1;
         return false;
     }
-    if (dup2(_newOut, fileno(stderr)) == -1) {
+
+    // if stderr has no underlying handle, use freopen
+    if (_oldErr < 0 || _newErr < 0) {
+        freopen(_logFile.c_str(), "w", stderr);
+        close(_newErr);
+        _newErr = fileno(stderr);
+        if (_newErr < 0) {
+            printf("Could not redirect stderr! Log may be incomplete!\n");
+        }
+    }
+    // otherwise have stderr file handle point to file
+    else if (dup2(_newErr, fileno(stderr)) == -1) {
         printf("Could not redirect stderr! Log may be incomplete!\n");
     }
+    
     fflush(stdout);
     fflush(stderr);
     return true;
 }
 void Log::UnredirectStdOut() {
-    if (_newOut == -1) return;
+    if (_newOut == -1 && _newErr == -1) return;
     fflush(stdout);
     fflush(stderr);
     close(_newOut);
+    close(_newErr);
     _newOut = -1;
-
-    dup2(_oldOut, fileno(stdout));
-    dup2(_oldErr, fileno(stderr));
-
-    close(_oldOut);
-    close(_oldErr);
+    _newErr = -1;
+    
+    if (_oldOut >= 0) {
+        dup2(_oldOut, fileno(stdout));
+        close(_oldOut);
+        _oldOut = -1;
+    } else {
+        // nothing to restore, just close underlying FD
+        close(fileno(stdout));
+    }
+    if (_oldErr >= 0) {
+        dup2(_oldErr, fileno(stderr));
+        close(_oldErr);
+        _oldErr = -1;
+    } else {
+        // nothing to restore, just close underlying FD
+        close(fileno(stderr));
+    }
 }
--- src/core/log.h
+++ src/core/log.h
@@ -1,16 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/* 
- * File:   Log.h
- * Author: andy
- *
- * Created on June 27, 2021, 11:15 AM
- */
-
 #ifndef LOG_H
 #define LOG_H
 
@@ -22,6 +9,7 @@ private:
     static int _oldOut;
     static int _oldErr;
     static int _newOut;
+    static int _newErr;
     
 public:
     static std::string getFile() { return _logFile; }
