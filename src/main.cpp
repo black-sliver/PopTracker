ad94e166d9ff6ed843a449f2e5c65bfd1d388775
black-sliver <>
2021-06-27 15:42:49 +0200

    Windows: print to promt if running from promt, --console switch to open a new console

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,28 @@
 #include <string.h>
 #include <stdio.h>
 
+
+#if defined WIN32 || defined _WIN32
+#include <windows.h>
+#endif
+
 int main(int argc, char** argv)
 {
+#if defined WIN32 || defined _WIN32
+    // use --console to force open a dos window
+    bool openConsole = false;
+    if (argc>1 && strcasecmp("--console", argv[1])==0) {
+        openConsole = true;
+        argv++;
+        argc--;
+    }
+    // enable stdout on windows
+    if(AttachConsole(ATTACH_PARENT_PROCESS) || (openConsole && AllocConsole())) {
+        freopen("CONOUT$", "w", stdout);
+        freopen("CONOUT$", "w", stderr);
+    }
+#endif
+
     if (argc>1 && strcasecmp("--version", argv[1])==0) {
         printf("%s\n", PopTracker::VERSION_STRING);
         return 0;
