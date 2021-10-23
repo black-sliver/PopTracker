5a94ef013dfef96c6fc507da0d3242a557cdfd3f
black-sliver <59490463+black-sliver@users.noreply.github.com>
2021-10-24 00:14:40 +0200

    add hosted_item example

---
+++ examples/hosted_item/README.md
@@ -0,0 +1,3 @@
+# hosted_item Example Pack
+
+This pack tries to show how to use hosted_items.
---
+++ examples/hosted_item/scripts/logic.lua
@@ -0,0 +1,7 @@
+function some_rule()
+    print("Testing some rule")
+    return Tracker:ProviderCountForCode("c") -- this is a number, not boolean!
+    -- you can do arithmetic:
+    -- addition `+` of two codes is logical Or
+    -- multiplication `*` of two codes is logical And
+end
