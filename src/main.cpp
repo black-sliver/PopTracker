64a49d1603a11def714f4d69d3abc7b4b971e009
black-sliver <59490463+black-sliver@users.noreply.github.com>
2022-02-03 20:30:55 +0100

    enable 'visual styles' on Windows

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,26 @@
 
 #if defined WIN32 || defined _WIN32
 #include <windows.h>
+
+// as per https://stackoverflow.com/questions/4308503
+ULONG_PTR EnableVisualStyles(VOID)
+{
+    TCHAR dir[MAX_PATH];
+    ULONG_PTR ulpActivationCookie = FALSE;
+    ACTCTX actCtx =
+    {
+        sizeof(actCtx),
+        ACTCTX_FLAG_RESOURCE_NAME_VALID
+            | ACTCTX_FLAG_SET_PROCESS_DEFAULT
+            | ACTCTX_FLAG_ASSEMBLY_DIRECTORY_VALID,
+        TEXT("shell32.dll"), 0, 0, dir, (LPCTSTR)124
+    };
+    UINT cch = GetSystemDirectory(dir, sizeof(dir) / sizeof(*dir));
+    if (cch >= sizeof(dir) / sizeof(*dir)) { return FALSE; /*shouldn't happen*/ }
+    dir[cch] = TEXT('\0');
+    ActivateActCtx(CreateActCtx(&actCtx), &ulpActivationCookie);
+    return ulpActivationCookie;
+}
 #endif
 
 int main(int argc, char** argv)
@@ -24,6 +44,7 @@ int main(int argc, char** argv)
         freopen("CONOUT$", "w", stderr);
         setvbuf(stderr, NULL, _IONBF, 0);
     }
+    EnableVisualStyles();
 #endif
 
     if (argc>1 && strcasecmp("--version", argv[1])==0) {
