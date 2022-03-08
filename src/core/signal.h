8f578a5813ffd630859585e4ad4839193824fe92
black-sliver <59490463+black-sliver@users.noreply.github.com>
2022-03-08 09:38:47 +0100

    allow modifying signal from callback

--- src/core/signal.h
+++ src/core/signal.h
@@ -5,6 +5,7 @@
 #include <functional>
 #include <algorithm>
 #include <stdio.h>
+#include <memory>
 
 template <typename ... Args>
 class Signal {
@@ -13,22 +14,35 @@ public:
     using F = std::function<void(void*, Args...)>;
     using PAIR = std::pair<O,F>;
     void emit(void* sender, Args... args) {
+        auto modified = _modified;
+        *modified = false;
         for (auto& slot: _slots) {
             slot.second(sender, args...);
+            if (*modified) break;
         }
     }
     void operator+=(PAIR pair) {
         //TODO: pair.first->onDelete += ..._slots.erase...
         _slots.push_back(pair);
+        *_modified = true;
     }
     void operator-=(O o) {
         _slots.erase(std::remove_if(_slots.begin(), _slots.end(), [o](PAIR const &pair) { return pair.first==o; }), _slots.end());
+        *_modified = true;
     }
     void operator-=(F f) {
         _slots.erase(std::remove_if(_slots.begin(), _slots.end(), [f](PAIR const &pair) { return pair.second==f; }), _slots.end());
+        *_modified = true;
+    }
+    Signal() {
+        _modified = std::make_shared<bool>(false);
+    }
+    virtual ~Signal() {
+        *_modified = true;
     }
 private:
     std::deque<PAIR> _slots;
+    std::shared_ptr<bool> _modified;
 };
 
 #endif // _CORE_SIGNAL_H
