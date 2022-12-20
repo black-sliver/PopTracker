bcc1cdb1028f4291dfa8ffb988ce17c6eb77fadb
black-sliver <59490463+black-sliver@users.noreply.github.com>
2022-12-20 23:36:50 +0100

    allow string-to-int for maps

--- src/core/map.cpp
+++ src/core/map.cpp
@@ -1,26 +1,21 @@
 #include "map.h"
+#include "jsonutil.h"
+
+
 using nlohmann::json;
 
-static std::string to_string(const json& j, const std::string& dflt)
-{
-    return (j.type() == json::value_t::string) ? j.get<std::string>() : dflt;
-}
-static int to_int(const json& j, int dflt)
-{
-    // TODO: float?
-    return (j.type() == json::value_t::number_integer) ? j.get<int>() : 
-           (j.type() == json::value_t::number_unsigned) ? (int)j.get<unsigned>() : dflt;
-}
 
 Map Map::FromJSONString(const std::string& j)
 {
     return FromJSON(json::parse(j, nullptr, true, true));
 }
+
 Map Map::FromJSON(json&& j)
 {
     auto tmp = j;
     return FromJSON(tmp);
 }
+
 Map Map::FromJSON(json& j)
 {
     Map map;
