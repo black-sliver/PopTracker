403b507a88e6f9f7491ba068826d2ff44ce16750
black-sliver <59490463+black-sliver@users.noreply.github.com>
2023-09-02 22:35:23 +0200

    LuaItem: better error messages

--- src/core/luaitem.cpp
+++ src/core/luaitem.cpp
@@ -282,11 +282,11 @@ void LuaItem::Set(const char* s, LuaVariant v)
     lua_pushstring(_L, s); // arg2: key
     v.Lua_Push(_L);        // arg3: value
     if (lua_pcall(_L, 3, 0, 0)) {
-        printf("Error calling Item:propertyChanged: %s\n", lua_tostring(_L, -1));
+        printf("Error calling Item:propertyChanged for \"%s\": %s\n",
+                sanitize_print(_name).c_str(), lua_tostring(_L, -1));
         lua_pop(_L, 1);
         return;
     }
-    // TODO: clean up lua stack
     DEBUG_printf("LuaItem(\"%s\"):propertyChanged(\"%s\",%s) called\n", _name.c_str(), s, v.toString().c_str());
 }
 
@@ -316,8 +316,9 @@ bool LuaItem::load(json& j)
     Lua_Push(_L); // arg1: this
     json_to_lua(_L, j); // arg2: data table
     if (lua_pcall(_L, 2, 0, 0)) {
-        printf("Error calling Item:load: %s\n", lua_tostring(_L, -1));
-        // TODO: clean up lua stack
+        printf("Error calling Item:load for \"%s\": %s\n",
+                sanitize_print(_name).c_str(), lua_tostring(_L, -1));
+        lua_pop(_L, 1);
         return false;
     }
     return true;
