d220e26ca393cdb443098b2de5eb0b5bdd65e7f2
black-sliver <59490463+black-sliver@users.noreply.github.com>
2023-12-29 18:05:21 +0100

    allow image mods for overlay image mod
    "img_mods" = "overlay|path/to/file|@disabled"

--- src/uilib/imagefilter.cpp
+++ src/uilib/imagefilter.cpp
@@ -4,23 +4,31 @@
 
 namespace Ui {
 
-SDL_Surface* ImageFilter::apply(SDL_Surface *surf)
+SDL_Surface* ImageFilter::apply(SDL_Surface *surf) const
 {
     if (!surf) return surf;
     
     if (name=="grey" || name=="disable") {
         return makeGreyscale(surf, true);
     }
-    if (name=="overlay" && !arg.empty()) {
-        auto overlay = IMG_Load_RW(SDL_RWFromMem((void*)arg.c_str(), (int)arg.length()), 1);
+    if (name=="overlay" && !args.empty()) {
+        auto overlay = IMG_Load_RW(SDL_RWFromMem((void*)args[0].c_str(), (int)args[0].length()), 1);
         overlay = makeTransparent(overlay, 0xff, 0x00, 0xff, false);
         if (overlay) {
+            bool first = true;
+            for (const auto& arg: args) {
+                if (first) {
+                    first = false; // skip the first arg (the image)
+                } else {
+                    overlay = ImageFilter(arg).apply(overlay);
+                }
+            }
             SDL_BlitSurface(overlay, nullptr, surf, nullptr);
             SDL_FreeSurface(overlay);
         }
         else {
             fprintf(stderr, "IMG_Load: %s\n", IMG_GetError());
-        }        
+        }
     }
     
     return surf;
--- src/uilib/imagefilter.h
+++ src/uilib/imagefilter.h
@@ -4,23 +4,36 @@
 
 #include "colorhelper.h"
 #include <string>
+#include <vector>
 #include <SDL2/SDL_image.h>
 
 
 namespace Ui {
 
 struct ImageFilter {
-    ImageFilter(std::string name) { this->name=name; }
-    ImageFilter(std::string name, std::string arg) { this->name=name; this->arg=arg; }
-    
+    ImageFilter(std::string name)
+        : name(name)
+    {
+    }
+
+    ImageFilter(std::string name, std::string arg)
+        : name(name), args({arg})
+    {
+    }
+
+    ImageFilter(std::string name, std::vector<std::string>& args)
+        : name(name), args(args)
+    {
+    }
+
     std::string name;
-    std::string arg;
+    std::vector<std::string> args;
     
-    SDL_Surface* apply(SDL_Surface *surf);
+    SDL_Surface* apply(SDL_Surface *surf) const;
 
     bool operator==(const ImageFilter& rhs) const
     {
-        return name == rhs.name && arg == rhs.arg;
+        return name == rhs.name && args == rhs.args;
     }
 };
 
