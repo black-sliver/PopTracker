b18482472821b6343260ddcfa139468110b8e9e5
black-sliver <59490463+black-sliver@users.noreply.github.com>
2024-01-24 21:40:45 +0100

    LoadPackWidget: make line stick when click

--- src/ui/loadpackwidget.h
+++ src/ui/loadpackwidget.h
@@ -34,6 +34,15 @@ protected:
     HBox *_main;
     Label *_curPackLabel = nullptr;
     Label *_curVariantLabel = nullptr;
+    Label *_curPackHover = nullptr;
+    bool _disableHoverSelect = false;
+
+    static constexpr Color PACK_BG_DEFAULT = {32, 32, 32};
+    static constexpr Color PACK_BG_ACTIVE = {32, 128, 32};
+    static constexpr Color PACK_BG_HOVER = {64, 64, 64};
+    static constexpr Color PACK_BG_ACTIVE_HOVER = {64, 160, 64};
+    static constexpr Color VARIANT_BG_DEFAULT = {64, 64, 64};
+    static constexpr Color VARIANT_BG_HOVER = {96, 96, 96};
 };
 
 } // namespace Ui
