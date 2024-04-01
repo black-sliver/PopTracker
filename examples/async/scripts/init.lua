fdb369cc952bb789db6ea534eb5eec5539874424
black-sliver <59490463+black-sliver@users.noreply.github.com>
2024-04-01 13:32:52 +0200

    Lua: add example for AsyncProgress

--- examples/async/scripts/init.lua
+++ examples/async/scripts/init.lua
@@ -7,3 +7,7 @@ Tracker:AddLayouts("layouts/standard.json")
 ScriptHost:RunScriptAsync("scripts/async.lua", "Hello, World!", function(result)
     print("Async result: " .. tostring(result))
 end)
+
+ScriptHost:RunStringAsync("ScriptHost:AsyncProgress('step 1'); ScriptHost:AsyncProgress('step 2')", nil, function() end, function(progress)
+    print("Async progress: " .. tostring(progress))
+end)
