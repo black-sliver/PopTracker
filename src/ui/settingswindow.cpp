ab68afe1633af8f4b6acfa64964aa3cf4ac06444
black-sliver <59490463+black-sliver@users.noreply.github.com>
2024-12-12 00:35:30 +0100

    Set default settings window min size to 100x100
    This works around layouts with no h-grow not properly shrinking the window.

--- src/ui/settingswindow.cpp
+++ src/ui/settingswindow.cpp
@@ -4,7 +4,7 @@
 namespace Ui {
 
 SettingsWindow::SettingsWindow(const char* title, SDL_Surface* icon, const Position& pos, const Size& size)
-    : TrackerWindow(title, icon, pos, size)
+    : TrackerWindow(title, icon, pos, {size.width < 1 ? 100 : size.width, size.height < 1 ? 100:size.height})
 {
     if (size.width<1 || size.height<1) resize({100,100});
 }
