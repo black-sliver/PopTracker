c3d5b3f601b4c7b36b0a7ffb9ab53423a6c7fa65
black-sliver <59490463+black-sliver@users.noreply.github.com>
2024-12-29 03:23:12 +0100

    win32: cross build: update libs

--- win32/cross-compile-libs-win32.sh
+++ win32/cross-compile-libs-win32.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-SDL_SRC=SDL2-2.30.2
-IMAGE_SRC=SDL2_image-2.8.2
+SDL_SRC=SDL2-2.30.10
+IMAGE_SRC=SDL2_image-2.8.4
 TTF_SRC=SDL2_ttf-2.22.0
 
 # Helper to cross compile SDL libs for Windows on Linux
