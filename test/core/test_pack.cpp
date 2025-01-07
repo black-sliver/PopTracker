9d1777fbc06183b4bceed444dc792f25d5772600
black-sliver <59490463+black-sliver@users.noreply.github.com>
2025-01-08 00:02:45 +0100

    Test: add tests for Pack::isInSearchPath

---
+++ test/core/test_pack.cpp
@@ -0,0 +1,53 @@
+#include <gtest/gtest.h>
+#include "../../src/core/fs.h"
+#include "../../src/core/pack.h"
+
+
+static fs::path my_realpath(const fs::path& path)
+{
+    fs::path res;
+#if !defined WIN32 && !defined _WIN32
+    char* tmp = realpath(path.c_str(), NULL);
+    if (tmp) {
+        res = tmp;
+        free(tmp);
+    }
+#else
+    auto tmp = _wfullpath(nullptr, path.c_str(), 1024);
+    if (tmp) {
+        auto cmp = [tmp](const fs::path& p) { return wcsicmp(tmp, p.c_str()) == 0; };
+        res = tmp;
+        free(tmp);
+    }
+#endif
+    return res;
+}
+
+
+TEST(PackTest, MissingNotInSearchPath) {
+    Pack::addSearchPath("assets"); // ./packs/ may not exist
+#if !defined WIN32 && !defined _WIN32
+    fs::path path = "/tmp/pack.zip";
+#else
+    fs::path path = fs::u8path("C:\\pack.zip");
+#endif
+    EXPECT_FALSE(Pack::isInSearchPath(path));
+}
+
+TEST(PackTest, RelNotInSearchPath) {
+    Pack::addSearchPath("assets"); // ./packs/ may not exist
+    fs::path path = fs::u8path("examples/async");
+    EXPECT_FALSE(Pack::isInSearchPath(path));
+}
+
+TEST(PackTest, RelIsInSearchPath) {
+    Pack::addSearchPath("examples");
+    fs::path path = fs::u8path("examples/async");
+    EXPECT_TRUE(Pack::isInSearchPath(path));
+}
+
+TEST(PackTest, AbsIsInSearchPath) {
+    Pack::addSearchPath("examples");
+    fs::path path = my_realpath(fs::u8path("examples/async"));
+    EXPECT_TRUE(Pack::isInSearchPath(path));
+}
