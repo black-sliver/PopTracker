0c81ed2eb78160fdafab354467ca95bed075b7a8
black-sliver <59490463+black-sliver@users.noreply.github.com>
2025-05-29 14:32:43 +0200

    Ui: tidy tabs

--- src/uilib/tabs.h
+++ src/uilib/tabs.h
@@ -1,14 +1,14 @@
-#ifndef _UILIB_TABS_H
-#define _UILIB_TABS_H
+#pragma once
 
 // TODO: ideally we would want an HBOX that has the first and last element as
 //       Spacers that just grow the same amount to center the buttons
 //       for now we just size everything "manually"
 
+#include <vector>
+#include "button.h"
 #include "container.h"
 #include "hflexbox.h"
-#include "button.h"
-#include <vector>
+
 
 namespace Ui {
 
@@ -16,12 +16,12 @@ class Tabs : public Container {
 public:
     using FONT = Button::FONT;
     Tabs(int x, int y, int w, int h, FONT font);
-    virtual ~Tabs();
-    
-    virtual void addChild(Widget* w) override;
-    virtual void removeChild(Widget* w) override;
-    virtual void render(Renderer renderer, int offX, int offY) override;
-    virtual void setSize(Size size) override;
+    ~Tabs() override;
+
+    void addChild(Widget* w) override;
+    void removeChild(Widget* w) override;
+    void render(Renderer renderer, int offX, int offY) override;
+    void setSize(Size size) override;
     virtual void setTabName(int index, const std::string& name);
     virtual void setTabIcon(int index, const void *data, size_t len);
     virtual bool setActiveTab(const std::string& name);
@@ -31,7 +31,7 @@ public:
     /// reserves memory for the tabs
     void reserve(size_t size);
 
-    virtual bool isHit(int x, int y) const override {
+    bool isHit(int x, int y) const override {
         return _buttonbox->isHit(x - _pos.left, y - _pos.top) || Container::isHit(x, y);
     }
 
@@ -47,5 +47,3 @@ protected:
 };
 
 } // namespace Ui
-
-#endif // _UILIB_TABS_H
