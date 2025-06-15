34a33cffdc0a7d6ef32db9e575751cd646fcf421
black-sliver <59490463+black-sliver@users.noreply.github.com>
2025-06-16 00:16:51 +0200

    Bump version to 0.32.1-rc1, update CHANGELOG

--- src/version.h
+++ src/version.h
@@ -6,11 +6,11 @@
 /// semver <minor> of the application
 #define APP_VERSION_MINOR     32
 /// semver <patch> of the application
-#define APP_VERSION_REVISION   0
+#define APP_VERSION_REVISION   1
 /// semver "-" <pre-release> and/or "+" <build> of the application
-#define APP_VERSION_EXTRA     ""
+#define APP_VERSION_EXTRA     "-rc1"
 /// set to 1 for any pre-release or 0 for release and post-release
-#define APP_VERSION_PRERELEASE 0
+#define APP_VERSION_PRERELEASE 1
 
 #ifndef XSTR
 #define XSTR(s) STR(s)
