b65d47ab95905c7a66698e3b66f17d0bd32551ce
black-sliver <59490463+black-sliver@users.noreply.github.com>
2025-07-23 00:57:09 +0200

    Packlist: make util follow links and better CI

--- util/validate-packs.py
+++ util/validate-packs.py
@@ -1,13 +1,29 @@
 #!/usr/bin/python
 
 import json
+from enum import Enum
+from typing import Any, Dict, Optional
 from urllib.request import urlopen
 
 import jsonschema
 
 
-def validate(instance, schema_filename=None, follow_links=False, versions_schema_filename=None, *,
-             instance_filename=None, verbose=False):
+class FollowMode(Enum):
+    NONE = 0
+    FIRST = 1
+    ALL = 2
+
+
+def validate(
+        instance: Any,
+        schema_filename: Optional[str] = None,
+        follow_links: bool = False,
+        versions_schema_filename: Optional[str] = None,
+        *,
+        follow_recursive: FollowMode = FollowMode.NONE,
+        instance_filename: Optional[str] = None,
+        verbose: bool = False
+) -> None:
     if not schema_filename:
         schema_filename = os.path.join(os.path.dirname(__file__), "../schema/packs.schema.json")
     if not versions_schema_filename:
@@ -17,6 +33,9 @@ def validate(instance, schema_filename=None, follow_links=False, versions_schema
             print(f"Validating {instance_filename or 'json'}")
         jsonschema.validate(instance=instance, schema=json.load(schema_file))
         if follow_links:
+            import importlib
+
+            versions_mod = importlib.import_module("validate-versions")
             with open(versions_schema_filename, "r") as versions_schema_file:
                 versions_schema = json.load(versions_schema_file)
             for uid, pack in instance.items():
@@ -26,10 +45,13 @@ def validate(instance, schema_filename=None, follow_links=False, versions_schema
                         print(f"Validating {uid} versions: {versions_url}")
                     versions_data = json.load(urlopen(versions_url))
                     jsonschema.validate(instance=versions_data, schema=versions_schema)
+                    if follow_recursive != FollowMode.NONE:
+                        for entry in versions_data["versions"]:
+                            if (versions_mod.validate_version_entry(entry, uid, verbose=verbose)
+                                    and follow_recursive == FollowMode.FIRST):
+                                break
                 except Exception as e:
                     raise ValueError(f"Error validating {versions_url} for {uid}") from e
-
-    return True
 
 
 if __name__ == "__main__":
@@ -44,9 +66,30 @@ if __name__ == "__main__":
                         help="schema to use for version.json files if --follow-links is used")
     parser.add_argument("--follow-links", action="store_true", dest="follow_links",
                         help="follow links and validate linked files.")
+    parser.add_argument("--recursive", action="store_true", dest="recursive",
+                        help="when combined with --follow-links, follows links in linked files.")
+    parser.add_argument("--only-first", action="store_true", dest="only_first",
+                        help="when combined with --recursive, only follows first entry of a list.")
+    parser.add_argument("--diff", type=argparse.FileType("r"), dest="diff",
+                        help="Only check changes from file passed for diff.")
     parser.add_argument("--verbose", action="store_true")
     args = parser.parse_args()
-    validate(json.load(args.infile), args.schema, args.follow_links, args.versions_schema,
-             instance_filename=args.infile.name,
-             verbose=args.verbose, )
+    recursive_mode = FollowMode.NONE if not args.recursive else FollowMode.FIRST if args.only_first else FollowMode.ALL
+    data: Dict[str, Any] = json.load(args.infile)
+    if args.diff:
+        orig: Dict[str, Any] = json.load(args.diff)
+        data = {
+            k: v
+            for k, v in data.items()
+            if k not in orig or v != orig[k]
+        }
+    validate(
+        data,
+        args.schema,
+        args.follow_links,
+        args.versions_schema,
+        follow_recursive=recursive_mode,
+        instance_filename=args.infile.name,
+        verbose=args.verbose,
+    )
     exit(0)
