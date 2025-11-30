4e0de30e08c89446a6f646752a74e226afcbdea0
black-sliver <59490463+black-sliver@users.noreply.github.com>
2025-11-30 16:10:23 +0100

    Bump version to 0.33.0-rc3, update CHANGELOG

--- src/version.h
+++ src/version.h
@@ -8,7 +8,7 @@
 /// semver <patch> of the application
 #define APP_VERSION_REVISION   0
 /// semver "-" <pre-release> and/or "+" <build> of the application
-#define APP_VERSION_EXTRA     "-rc2"
+#define APP_VERSION_EXTRA     "-rc3"
 /// set to 1 for any pre-release or 0 for release and post-release
 #define APP_VERSION_PRERELEASE 1
 
