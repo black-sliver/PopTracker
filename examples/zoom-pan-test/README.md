1f8ced592c61ed34c87f158444cea8c452c41839
black-sliver <59490463+black-sliver@users.noreply.github.com>
2026-03-25 21:53:42 +0100

    Examples: add zoom/pan Lua UiHint example

---
+++ examples/zoom-pan-test/README.md
@@ -0,0 +1,7 @@
+# PopTracker Zoom/Pan Example Pack
+
+This example shows how to use Zoom and Pan UI Hints.
+
+* A resets the Map
+* B cycles through pan positions
+* C cycles through zoom levels
---
+++ examples/zoom-pan-test/scripts/init.lua
@@ -0,0 +1,9 @@
+-- Zoom/Pan test pack by black_sliver
+-- init.lua
+
+Tracker:AddMaps("maps/maps.json")
+Tracker:AddLocations("locations/locations.json")
+Tracker:AddItems("items/items.json")
+Tracker:AddLayouts("layouts/standard.json")
+
+require "actions"
