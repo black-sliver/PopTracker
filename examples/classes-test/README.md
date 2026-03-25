514956b23207509170d65d42f20a7af69b03e5cc
black-sliver <59490463+black-sliver@users.noreply.github.com>
2026-03-25 22:03:19 +0100

    Examples, CI: add pack showing "class" usage

---
+++ examples/classes-test/README.md
@@ -0,0 +1,3 @@
+# PopTracker Classes Test Pack
+
+This pack may one day become a regression test for layout classes.
---
+++ examples/classes-test/scripts/init.lua
@@ -0,0 +1,6 @@
+--items
+Tracker:AddItems("items/items.json")
+--layout classes
+Tracker:AddClasses("classes/classes.json")
+--layouts
+Tracker:AddLayouts("layouts/tracker.json")
