62d791183b5aca146105a8cae3b3d00d61431776
black-sliver <59490463+black-sliver@users.noreply.github.com>
2026-09-19 00:10:17 +0200

    Sanitize key in missing layouts error, reformat

--- src/ui/trackerview.cpp
+++ src/ui/trackerview.cpp
@@ -1115,13 +1115,10 @@ bool TrackerView::addLayoutNode(Container* container, const LayoutNode& node, si
     }
     else if (node.getType() == "layout") {
         _layoutRefs.push_back(node.getKey());
-        if (_tracker->hasLayout(node.getKey())) {
+        if (_tracker->hasLayout(node.getKey()))
             return addLayoutNode(container, _tracker->getLayout(node.getKey()), depth+1);
-        }
-        else {
-            fprintf(stderr, "Missing layout for: %s\n", node.getKey().c_str());
-            return false;
-        }
+        fprintf(stderr, "Missing layout for: %s\n", sanitize_print(node.getKey()).c_str());
+        return false;
     }
     else if (node.getType() == "text") {
         Label *w = new Label(node.getPosition().x, node.getPosition().y, 0, 0, _font, node.getText());
