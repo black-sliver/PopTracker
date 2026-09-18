637908bb3563dad0810c823d71dffca5103bd4e3
Oshroth <Oshroth@users.noreply.github.com>
2026-09-19 08:35:07 +1000

    Added a more descriptive error message for missing layouts (#595)
    * Added a more descriptive error message for missing layouts
    
    * Sanitize key in missing layouts error, reformat
    
    ---------
    
    Co-authored-by: Oshroth <10400354+Oshroth@users.noreply.github.com>
    Co-authored-by: black-sliver <59490463+black-sliver@users.noreply.github.com>

--- src/ui/trackerview.cpp
+++ src/ui/trackerview.cpp
@@ -1115,7 +1115,10 @@ bool TrackerView::addLayoutNode(Container* container, const LayoutNode& node, si
     }
     else if (node.getType() == "layout") {
         _layoutRefs.push_back(node.getKey());
-        return addLayoutNode(container, _tracker->getLayout(node.getKey()), depth+1);
+        if (_tracker->hasLayout(node.getKey()))
+            return addLayoutNode(container, _tracker->getLayout(node.getKey()), depth+1);
+        fprintf(stderr, "Missing layout for: %s\n", sanitize_print(node.getKey()).c_str());
+        return false;
     }
     else if (node.getType() == "text") {
         Label *w = new Label(node.getPosition().x, node.getPosition().y, 0, 0, _font, node.getText());
