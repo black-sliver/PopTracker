e385caf4d339501a8bee3834ec5be9c956a84fe8
black-sliver <59490463+black-sliver@users.noreply.github.com>
2026-09-20 01:16:04 +0200

    build, macos: fix shellcheck warnings

--- macosx/build_thirdparty.sh
+++ macosx/build_thirdparty.sh
@@ -3,6 +3,8 @@
 # script to fetch and build third party sources.
 # (c) 2022 sbzappa
 
+# shellcheck disable=SC2001
+
 CLEAR_BUILD_DIR=no
 BUILD_DIR="build"
 CLEAR_LIB_DIR=no
@@ -13,12 +15,12 @@ DEPLOYMENT_TARGET="10.12"
 #WITH_PNG=true  # only required as dependency of freetype
 
 function usage() {
-  echo "Usage: `basename $0` [--build-dir=] [--clear-build-dir] [--lib-dir=] [--clear-lib-dir] [--deployment-target=]"
+  echo "Usage: $(basename "$0") [--build-dir=] [--clear-build-dir] [--lib-dir=] [--clear-lib-dir] [--deployment-target=]"
 }
 
 while test $# -gt 0; do
   case "$1" in
-  -*=*) optarg=`echo "$1" | sed 's/[-_a-zA-Z0-9]*=//'` ;;
+  -*=*) optarg=$(echo "$1" | sed 's/[-_a-zA-Z0-9]*=//') ;;
   *) optarg= ;;
   esac
 
@@ -46,8 +48,8 @@ while test $# -gt 0; do
   shift
 done
 
-SRC_DIR=`dirname $0`
-cd $SRC_DIR
+SRC_DIR=$(dirname "$0")
+cd "$SRC_DIR" || exit 1
 
 LIB_SDL_URL="https://github.com/libsdl-org/SDL.git"
 LIB_SDL_IMAGE_URL="https://github.com/libsdl-org/SDL_image.git"
@@ -76,41 +78,42 @@ LIB_SDL_IMAGE_FEATURE_FLAGS="--disable-sdltest --disable-stb-image --enable-bmp 
 
 REGEX="s/^.*\/\([^\/]*\).git$/\1/"
 
-LIB_SDL_DEST_DIR="$BUILD_DIR/`echo $LIB_SDL_URL | sed $REGEX`"
-LIB_SDL_IMAGE_DEST_DIR="$BUILD_DIR/`echo $LIB_SDL_IMAGE_URL | sed $REGEX`"
-LIB_SDL_TTF_DEST_DIR="$BUILD_DIR/`echo $LIB_SDL_TTF_URL | sed $REGEX`"
-LIB_PNG_DEST_DIR="$BUILD_DIR/`echo $LIB_PNG_URL | sed $REGEX`"
-LIB_FREETYPE_DEST_DIR="$BUILD_DIR/`echo $LIB_FREETYPE_URL | sed $REGEX`"
-LIB_OPENSSL_DEST_DIR="$BUILD_DIR/`echo $LIB_OPENSSL_URL | sed $REGEX`"
+LIB_SDL_DEST_DIR="$BUILD_DIR/$(echo "$LIB_SDL_URL" | sed "$REGEX")"
+LIB_SDL_IMAGE_DEST_DIR="$BUILD_DIR/$(echo "$LIB_SDL_IMAGE_URL" | sed "$REGEX")"
+LIB_SDL_TTF_DEST_DIR="$BUILD_DIR/$(echo "$LIB_SDL_TTF_URL" | sed "$REGEX")"
+LIB_PNG_DEST_DIR="$BUILD_DIR/$(echo "$LIB_PNG_URL" | sed "$REGEX")"
+LIB_FREETYPE_DEST_DIR="$BUILD_DIR/$(echo "$LIB_FREETYPE_URL" | sed "$REGEX")"
+LIB_OPENSSL_DEST_DIR="$BUILD_DIR/$(echo "$LIB_OPENSSL_URL" | sed "$REGEX")"
 
 
 DEPLOYMENT_TARGET_FLAG="-mmacosx-version-min=$DEPLOYMENT_TARGET"
 CONFIGURE_FLAGS="CXXFLAGS=$DEPLOYMENT_TARGET_FLAG CFLAGS=$DEPLOYMENT_TARGET_FLAG LDFLAGS=$DEPLOYMENT_TARGET_FLAG"
 
 if test $CLEAR_BUILD_DIR = yes ; then
-  rm -fr $BUILD_DIR
+  rm -fr "$BUILD_DIR"
 fi
 
 
 if test $CLEAR_LIB_DIR = yes ; then
-  rm -fr $LIB_DIR
+  rm -fr "$LIB_DIR"
 fi
 
-mkdir -p $BUILD_DIR
-mkdir -p $LIB_DIR
+mkdir -p "$BUILD_DIR"
+mkdir -p "$LIB_DIR"
 
 if [ "$WITH_PNG" ]; then
     # Build Png
 
-    if [ ! -d $LIB_PNG_DEST_DIR ]; then
-      git clone --depth 1 --branch $LIB_PNG_TAG $LIB_PNG_URL $LIB_PNG_DEST_DIR || { echo "Could not clone $LIB_PNG_URL" ; exit 1; }
-      cd $LIB_PNG_DEST_DIR
+    if [ ! -d "$LIB_PNG_DEST_DIR" ]; then
+      git clone --depth 1 --branch "$LIB_PNG_TAG" "$LIB_PNG_URL" "$LIB_PNG_DEST_DIR" || { echo "Could not clone $LIB_PNG_URL" ; exit 1; }
+      cd "$LIB_PNG_DEST_DIR" || exit 1
     else
-      cd $LIB_PNG_DEST_DIR
+      cd "$LIB_PNG_DEST_DIR" || exit 1
       git pull
-      git checkout $LIB_PNG_TAG
+      git checkout "$LIB_PNG_TAG"
     fi
 
+    # shellcheck disable=SC2086
     ./configure $CONFIGURE_FLAGS
     make -j3
 
@@ -123,20 +126,21 @@ fi
 if [ "$WITH_FREETYPE" ]; then
     # Build Freetype
 
-    if [ ! -d $LIB_FREETYPE_DEST_DIR ]; then
-      git clone --depth 1 --branch $LIB_FREETYPE_TAG $LIB_FREETYPE_URL $LIB_FREETYPE_DEST_DIR || { echo "Could not clone $LIB_FREETYPE_URL" ; exit 1; }
-      cd $LIB_FREETYPE_DEST_DIR
+    if [ ! -d "$LIB_FREETYPE_DEST_DIR" ]; then
+      git clone --depth 1 --branch "$LIB_FREETYPE_TAG" "$LIB_FREETYPE_URL" "$LIB_FREETYPE_DEST_DIR" || { echo "Could not clone $LIB_FREETYPE_URL" ; exit 1; }
+      cd "$LIB_FREETYPE_DEST_DIR" || exit 1
     else
-      cd $LIB_FREETYPE_DEST_DIR
+      cd "$LIB_FREETYPE_DEST_DIR" || exit 1
       git pull
-      git checkout $LIB_FREETYPE_TAG
+      git checkout "$LIB_FREETYPE_TAG"
     fi
 
     ./autogen.sh
+    # shellcheck disable=SC2086
     ./configure $CONFIGURE_FLAGS --with-harfbuzz=no --with-brotli=no --with-png=yes
     make -j3
 
-    [ -f $LIB_FREETYPE_TARGET ] || { echo "Missing $LIB_FREETYPE_TARGET..." ; exit 1; }
+    [ -f "$LIB_FREETYPE_TARGET" ] || { echo "Missing $LIB_FREETYPE_TARGET..." ; exit 1; }
 
     cp "$LIB_FREETYPE_TARGET" "../../$LIB_DIR"
     cd "../.."
@@ -144,37 +148,39 @@ fi
 
 # Build SDL
 
-if [ ! -d $LIB_SDL_DEST_DIR ]; then
-  git clone --depth 1 --branch $LIB_SDL_TAG $LIB_SDL_URL $LIB_SDL_DEST_DIR || { echo "Could not clone $LIB_SDL_URL" ; exit 1; }
-  cd $LIB_SDL_DEST_DIR
+if [ ! -d "$LIB_SDL_DEST_DIR" ]; then
+  git clone --depth 1 --branch "$LIB_SDL_TAG" "$LIB_SDL_URL" "$LIB_SDL_DEST_DIR" || { echo "Could not clone $LIB_SDL_URL" ; exit 1; }
+  cd "$LIB_SDL_DEST_DIR" || exit 1
 else
-  cd $LIB_SDL_DEST_DIR
+  cd "$LIB_SDL_DEST_DIR" || exit 1
   git pull
-  git checkout $LIB_SDL_TAG
+  git checkout "$LIB_SDL_TAG"
 fi
 
 ./autogen.sh
+# shellcheck disable=SC2086
 ./configure $CONFIGURE_FLAGS
 make -j3
 
-[ -f $LIB_SDL_TARGET ] || { echo "Missing $LIB_SDL_TARGET..." ; exit 1; }
+[ -f "$LIB_SDL_TARGET" ] || { echo "Missing $LIB_SDL_TARGET..." ; exit 1; }
 
 cp "$LIB_SDL_TARGET" "../../$LIB_DIR"
 cd "../.."
 
 # Build SDL_image
 
-if [ ! -d $LIB_SDL_IMAGE_DEST_DIR ]; then
-  git clone --recurse-submodules --depth 1 --branch $LIB_SDL_IMAGE_TAG $LIB_SDL_IMAGE_URL $LIB_SDL_IMAGE_DEST_DIR || { echo "Could not clone $LIB_SDL_IMAGE_URL" ; exit 1; }
-  cd $LIB_SDL_IMAGE_DEST_DIR
+if [ ! -d "$LIB_SDL_IMAGE_DEST_DIR" ]; then
+  git clone --recurse-submodules --depth 1 --branch "$LIB_SDL_IMAGE_TAG" "$LIB_SDL_IMAGE_URL" "$LIB_SDL_IMAGE_DEST_DIR" || { echo "Could not clone $LIB_SDL_IMAGE_URL" ; exit 1; }
+  cd "$LIB_SDL_IMAGE_DEST_DIR" || exit 1
 else
-  cd $LIB_SDL_IMAGE_DEST_DIR
+  cd "$LIB_SDL_IMAGE_DEST_DIR" || exit 1
   git pull
-  git checkout $LIB_SDL_IMAGE_TAG
+  git checkout "$LIB_SDL_IMAGE_TAG"
   git submodule update --recursive
 fi
 
 ./autogen.sh
+# shellcheck disable=SC2086
 ./configure $CONFIGURE_FLAGS $LIB_SDL_IMAGE_FEATURE_FLAGS
 make -j3
 
@@ -185,43 +191,44 @@ cd "../.."
 
 # Build SDL_ttf (presumes freetype is already installed with brew)
 
-if [ ! -d $LIB_SDL_TTF_DEST_DIR ]; then
-  git clone --recurse-submodules --depth 1 --branch $LIB_SDL_TTF_TAG $LIB_SDL_TTF_URL $LIB_SDL_TTF_DEST_DIR || { echo "Could not clone $LIB_SDL_TTF_URL" ; exit 1; }
-  cd $LIB_SDL_TTF_DEST_DIR
+if [ ! -d "$LIB_SDL_TTF_DEST_DIR" ]; then
+  git clone --recurse-submodules --depth 1 --branch "$LIB_SDL_TTF_TAG" "$LIB_SDL_TTF_URL" "$LIB_SDL_TTF_DEST_DIR" || { echo "Could not clone $LIB_SDL_TTF_URL" ; exit 1; }
+  cd "$LIB_SDL_TTF_DEST_DIR" || exit 1
 else
-  cd $LIB_SDL_TTF_DEST_DIR
+  cd "$LIB_SDL_TTF_DEST_DIR" || exit 1
   git pull
-  git checkout $LIB_SDL_TTF_TAG
+  git checkout "$LIB_SDL_TTF_TAG"
   git submodule update --recursive
 fi
 
 ./autogen.sh
+# shellcheck disable=SC2086
 ./configure $CONFIGURE_FLAGS
 make -j3
 
-[ -f $LIB_SDL_TTF_TARGET ] || { echo "Missing $LIB_SDL_TTF_TARGET..." ; exit 1; }
+[ -f "$LIB_SDL_TTF_TARGET" ] || { echo "Missing $LIB_SDL_TTF_TARGET..." ; exit 1; }
 
 cp "$LIB_SDL_TTF_TARGET" "../../$LIB_DIR"
 cd "../.."
 
 # Build OpenSSL
 
-if [ ! -d $LIB_OPENSSL_DEST_DIR ]; then
-  git clone --depth 1 --branch $LIB_OPENSSL_TAG $LIB_OPENSSL_URL $LIB_OPENSSL_DEST_DIR || { echo "Could not clone $LIB_OPENSSL_URL" ; exit 1; }
-  cd $LIB_OPENSSL_DEST_DIR
+if [ ! -d "$LIB_OPENSSL_DEST_DIR" ]; then
+  git clone --depth 1 --branch "$LIB_OPENSSL_TAG" "$LIB_OPENSSL_URL" "$LIB_OPENSSL_DEST_DIR" || { echo "Could not clone $LIB_OPENSSL_URL" ; exit 1; }
+  cd "$LIB_OPENSSL_DEST_DIR" || exit 1
 else
-  cd $LIB_OPENSSL_DEST_DIR
+  cd "$LIB_OPENSSL_DEST_DIR" || exit 1
   git pull
-  git checkout $LIB_OPENSSL_TAG
+  git checkout "$LIB_OPENSSL_TAG"
 fi
 
+# shellcheck disable=SC2086
 ./config $CONFIGURE_FLAGS
 make -j4
 
-[ -f $LIB_OPENSSL_TARGET ] || { echo "Missing $LIB_OPENSSL_TARGET..." ; exit 1; }
-[ -f $LIB_CRYPTO_TARGET ] || { echo "Missing $LIB_CRYPTO_TARGET..." ; exit 1; }
+[ -f "$LIB_OPENSSL_TARGET" ] || { echo "Missing $LIB_OPENSSL_TARGET..." ; exit 1; }
+[ -f "$LIB_CRYPTO_TARGET" ] || { echo "Missing $LIB_CRYPTO_TARGET..." ; exit 1; }
 
 cp "$LIB_OPENSSL_TARGET" "../../$LIB_DIR"
 cp "$LIB_CRYPTO_TARGET" "../../$LIB_DIR"
 cd "../.."
-
