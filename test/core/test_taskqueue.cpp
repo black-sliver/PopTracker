21637d668f921cf71c19a573489315b0b45faba1
black-sliver <59490463+black-sliver@users.noreply.github.com>
2026-09-25 09:47:27 +0200

    Test: task in TaskQueue.AutoCancel may be done already

--- test/core/test_taskqueue.cpp
+++ test/core/test_taskqueue.cpp
@@ -170,7 +170,6 @@ TEST(TaskQueue, AutoCancel) {
         throw;
     }
     const auto capture = testing::internal::GetCapturedStderr();
-    EXPECT_TRUE(capture.find("2 queued tasks") != std::string::npos
-        && capture.find("2 pending results") != std::string::npos)
+    EXPECT_TRUE(capture.find("2 pending results") != std::string::npos)
         << "Expected warning on stderr, got " << capture;
 }
